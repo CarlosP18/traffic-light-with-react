16a920ef9c8bd67eac50e0816b7f9e5ba486714f
Carlos Plaza <carlosfplaza@hotmail.com>
2020-11-03 17:33:37 -0300

    se agrega state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const { state, changeState } = useState({
+    glowingLight: null,
+    colors: [{ color: 'red' }, { color: 'yellow' }, { color: 'green' }]
+  });
+
+  function toggleActive(index) {
+    changeState({ ...state, glowingLight: state.colors[index] })
+  }
+
+  function toggleGlowStyles(index) {
+    if (state.colors[index] === state.glowingLight) {
+      return "glow";
+    } else {
+      return "notglow"
+    }
+  }
 
   return (
     <>
       <div className="post"></div>
       <div className="trafficLight">
-        <div  id="redLight "></div>
-        <div  id="yellowLight"></div>
-        <div  id="greenLight"></div>
+        {state.colors.map((elements, index) => (
+          <div key={index} className={`${elements.color}  ${toggleGlowStyles(index)}`} onClick={() => { toggleActive(index); }} ></div>
+        ))}
       </div>
     </>
   );
